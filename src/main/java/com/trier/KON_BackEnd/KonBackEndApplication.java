27d29993234387b7403747e7efb364afce44c412
RochaGR <gutop.rocha@gmail.com>
2025-11-12 16:35:56 -0300

    commit inicial

---
+++ src/main/java/com/trier/KON_BackEnd/KonBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.trier.KON_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trier/KON_BackEnd/KonBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trier.KON_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
