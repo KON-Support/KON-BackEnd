1bf9aac5e8d6d9a8ebd29dc15c410d49c31245ff
Luiz Henrique <lhmendes0911@gmail.com>
2025-11-13 14:39:31 -0300

    feat: criando campos dtCriacao e hrCriacao, criando metodo findAllByCdUsuario no repository

--- src/main/java/com/trier/KON_BackEnd/dto/request/ComentarioRequestDTO.java
+++ src/main/java/com/trier/KON_BackEnd/dto/request/ComentarioRequestDTO.java
@@ -1,24 +1,20 @@
 package com.trier.KON_BackEnd.dto.request;
 
+import com.trier.KON_BackEnd.model.ChamadoModel;
+import com.trier.KON_BackEnd.model.Usuario;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.time.LocalDateTime;
-
-
 public record ComentarioRequestDTO(
 
-        Long cdComentario,
+        @NotNull(message = "O código do chamado é obrigatório.")
+        ChamadoModel cdChamado,
 
-        @NotNull(message = "O campo 'cdChamado' é obrigatório.")
-        Chamado cdChamado,
-
-        @NotNull(message = "O campo 'cdUsuario' é obrigatório.")
+        @NotNull(message = "O código de Usuário é obrigatório.")
         Usuario cdUsuario,
 
-        @Size(min = 2, max = 1000, message = "O campo 'dsConteudo' deve ter entre 2 e 1000 caracteres.")
-        String dsConteudo,
+        @Size(min = 2, max = 1000, message = "O conteúdo deve ter entre 2 e 1000 caracteres.")
+        String dsConteudo
 
-        LocalDateTime dtCriacao
 ) {
 }
