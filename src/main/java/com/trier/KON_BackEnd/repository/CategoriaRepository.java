da7b05a4ec59b7404e9f568728bdc5f20e985179
rafadevgg <rafaelpc5671@gmail.com>
2025-11-13 14:53:13 -0300

    Feat: Criando & Finalizando a classe service

--- src/main/java/com/trier/KON_BackEnd/repository/CategoriaRepository.java
+++ src/main/java/com/trier/KON_BackEnd/repository/CategoriaRepository.java
@@ -2,11 +2,16 @@ package com.trier.KON_BackEnd.repository;
 
 import com.trier.KON_BackEnd.model.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
 
     Optional<CategoriaModel> findByName(String nmCategoria);
+
+    @Query("select p from CategoriaModel p where p.flAtivo = true")
+    List<CategoriaModel> findAllByFlAtivo();
 
 }
---
+++ src/main/java/com/trier/KON_BackEnd/services/CategoriaService.java
@@ -0,0 +1,119 @@
+package com.trier.KON_BackEnd.services;
+
+import com.trier.KON_BackEnd.dto.request.CategoriaRequestDTO;
+import com.trier.KON_BackEnd.dto.response.CategoriaResponseDTO;
+import com.trier.KON_BackEnd.model.CategoriaModel;
+import com.trier.KON_BackEnd.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional
+    public CategoriaResponseDTO cadastrarCategoria(CategoriaRequestDTO categoriaRequest) {
+
+        CategoriaModel categoria = new CategoriaModel();
+
+        categoria.setNmCategoria(categoriaRequest.nmCategoria());
+        categoria.setFlAtivo(categoriaRequest.flAtivo());
+
+        categoriaRepository.save(categoria);
+
+        return new CategoriaResponseDTO(
+
+                categoria.getCdCategoria(),
+                categoria.getNmCategoria(),
+                categoria.getFlAtivo()
+
+        );
+
+    }
+
+    @Transactional
+    public CategoriaResponseDTO atualizarCategoria(Long cdCategoria , CategoriaRequestDTO categoriaRequest) {
+
+        var categoria = categoriaRepository.findById(cdCategoria)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada!"));
+
+        categoria.setNmCategoria(categoriaRequest.nmCategoria());
+
+        categoriaRepository.save(categoria);
+
+        return new CategoriaResponseDTO(
+
+                categoria.getCdCategoria(),
+                categoria.getNmCategoria(),
+                categoria.getFlAtivo()
+
+        );
+
+    }
+
+    @Transactional
+    public CategoriaResponseDTO desativarCategoria(Long cdCategoria) {
+
+        var categoria = categoriaRepository.findById(cdCategoria)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada!"));
+
+        categoria.setFlAtivo(false);
+
+        categoriaRepository.save(categoria);
+
+        return new CategoriaResponseDTO(
+
+                categoria.getCdCategoria(),
+                categoria.getNmCategoria(),
+                categoria.getFlAtivo()
+
+        );
+
+    }
+
+    @Transactional
+    public CategoriaResponseDTO listarCategoria(Long cdCategoria) {
+
+        var categoria = categoriaRepository.findById(cdCategoria)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+
+        return convertToResponseDTO(categoria);
+
+    }
+
+    @Transactional
+    public List<CategoriaResponseDTO> listarCategoriasAtivas() {
+
+        List<CategoriaModel> categoria = categoriaRepository.findAllByFlAtivo();
+
+        return categoria.stream().map(this:: convertToResponseDTO).toList();
+
+    }
+
+    @Transactional
+    public List<CategoriaResponseDTO> listarTodasCategorias() {
+
+        List<CategoriaModel> categoria = categoriaRepository.findAll();
+
+        return categoria.stream().map(this:: convertToResponseDTO).toList();
+
+    }
+
+    private CategoriaResponseDTO convertToResponseDTO(CategoriaModel categoria) {
+
+        return new CategoriaResponseDTO(
+
+                categoria.getCdCategoria(),
+                categoria.getNmCategoria(),
+                categoria.getFlAtivo()
+
+        );
+
+    }
+
+}
