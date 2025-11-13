f7993e6c2fb28459a618d3787eac854d4d670cad
rafadevgg <rafaelpc5671@gmail.com>
2025-11-13 13:32:14 -0300

    Feat: Criando toda estrutura da classe!

---
+++ src/main/java/com/trier/KON_BackEnd/model/CategoriaModel.java
@@ -0,0 +1,28 @@
+package com.trier.KON_BackEnd.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TBCATEGORIA")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class CategoriaModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDCATEGORIA")
+    private Long cdCategoria;
+
+    @Column(name = "NMCATEGORIA")
+    private String nmCategoria;
+
+    @Column(name = "FLATIVO")
+    private Boolean flAtivo;
+
+}
